d2c28af97282353540640818387ff63f0b27d93f
Rakesh Pavuluri <rakeshpavuluri@Rakeshs-MacBook-Pro.local>
2021-08-04 15:30:55 -0700

    Astra Org change to OIBA_ImageSearch

--- src/AstraDetails.js
+++ src/AstraDetails.js
@@ -1,4 +1,4 @@
-export const ASTRA_DB_ID = "3d0ec803-5233-4844-92b6-2d666f44f92b";
+export const ASTRA_DB_ID = "420db406-a317-4ac4-a914-22c734a73ff0";
 export const ASTRA_DB_REGION = "us-west-2";
 export const ASTRA_DB_KEYSPACE = "img_search";
 export const ASTRA_DB_TABLE = "images";
